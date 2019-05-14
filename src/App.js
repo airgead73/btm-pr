e41ef16fc08ccb82eea37b3615d9b77fd3e5c800
Dan Moneypenny <catullus1973@gmail.com>
2019-05-13 22:08:19 -0700

    Begin routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-
 
 class App extends Component {
   constructor(props) {
@@ -12,43 +10,10 @@ class App extends Component {
     }
   }  
 
-  componentDidMount() {
-    return axios.get('https://us-central1-btm-pr.cloudfunctions.net/getImages').then((response) => {
-      this.setState({
-        currentImages: response.data
-      })
-    })
-  }  
-  
-  renderItems() {
-    
-    const { currentImages, message } = this.state;
-
-    return (
-      currentImages.length > 0 &&
-      <div>
-                  
-          {
-            currentImages.map(item => {
-              return (
-
-                <figure key={item.id}>
-                  <img src={item.src} alt={item.alt} title={item.alt}/>
-                  <figcaption>{item.title}</figcaption>
-                </figure>
-              )
-            })
-          }
-        </div> 
-    )
-  }   
-  
-
   render() {
     return (
       <div>
-        <h1>Images</h1>
-        {this.renderItems()}
+        <h1>Brian's Work</h1>
       </div>
     );
   }
