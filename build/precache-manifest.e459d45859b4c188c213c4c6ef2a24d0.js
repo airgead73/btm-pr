e9fa6aa4c436fd7fb0fe12928b8ca3c9a36c8062
Dan Moneypenny <catullus1973@gmail.com>
2019-07-05 11:28:11 -0700

    update master

---
+++ build/precache-manifest.e459d45859b4c188c213c4c6ef2a24d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2ba220f7382832b11ff83c52f790e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b90c672e5468427b1bca",
+    "url": "/static/css/main.4a9357c8.chunk.css"
+  },
+  {
+    "revision": "cd534581e0bbe446205c",
+    "url": "/static/js/2.22e1eae2.chunk.js"
+  },
+  {
+    "revision": "b90c672e5468427b1bca",
+    "url": "/static/js/main.c2196d6c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
