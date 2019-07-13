57c03af0175405e930eb48967464fd69606152ff
Dan Moneypenny <catullus1973@gmail.com>
2019-07-13 09:59:04 -0700

    add webpack config

---
+++ app/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './style.scss';
+
+console.log('process.env.VERSION', process.env.VERSION);
+console.log('process.env.PLATFORM', process.env.PLATFORM);
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+
+const App = () => {
+  return (
+    <div>
+      <p>
+        We are a most promising species, Mr. Spock, as predators go. Did you know that? I frequently
+        have my doubts. I dont. Not any more. And maybe in a thousand years or so, we will be able
+        to prove it.
+      </p>
+      <p>- Captain Kirk</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
