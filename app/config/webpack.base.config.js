8a38e2a3bdcbe0bc9dbc59d8dda278a0312f6e40
Dan Moneypenny <catullus1973@gmail.com>
2019-07-14 17:06:59 -0700

    add bootstrap and jquery

--- app/config/webpack.base.config.js
+++ app/config/webpack.base.config.js
@@ -1,19 +1,20 @@
-const path = require('path');
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const MiniCssExtractPlugin =require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 
 module.exports = env => {
   const { PLATFORM, VERSION } = env;
   return merge([
     {
-      entry: './app/src/index.js',
+      entry: "./app/src/index.js",
       output: {
-        path: path.resolve(__dirname, '../dist'),
-        filename: 'main.js'
+        path: path.resolve(__dirname, "../dist"),
+        filename: "main.js"
       },
       module: {
         rules: [
@@ -21,45 +22,52 @@ module.exports = env => {
             test: /\.js$/,
             exclude: /node_modules/,
             use: {
-              loader: 'babel-loader'
+              loader: "babel-loader"
             }
           },
           {
-            test:/\.scss$/,
+            test: /\.(css|scss)$/,
             use: [
-              PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
-              'css-loader',
-              'sass-loader'
+              PLATFORM === "production"
+                ? MiniCssExtractPlugin.loader
+                : "style-loader",
+              "css-loader",
+              "sass-loader"
             ]
           },
-           {
-             test: /\.(png|svg|jpg|gif)$/,
-             use: {
-               loader: 'file-loader',
-               options: {
-                 name: 'images/[name].[ext]'
-                 //context: 'app/src',
-                 //publicPath: '/'
-               }
-             }
-           }
+          // {
+          //   test: /\.css$/,
+          //   use: ['style-loader', 'css-loader']
+          // },
+          {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: {
+              loader: "file-loader",
+              options: {
+                name: "images/[name].[ext]"
+                //context: 'app/src',
+                //publicPath: '/'
+              }
+            }
+          }
         ]
       },
       plugins: [
         new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({ 
-          template: './app/src/index.html', 
-          filename: 'index.html' 
+        new HtmlWebpackPlugin({
+          template: "./app/src/index.html",
+          filename: "index.html"
         }),
         new webpack.DefinePlugin({
-          'process.env.VERSION': JSON.stringify(env.VERSION),
-          'process.env.PLATFORM': JSON.stringify(env.PLATFORM)
+          "process.env.VERSION": JSON.stringify(env.VERSION),
+          "process.env.PLATFORM": JSON.stringify(env.PLATFORM)
         }),
-        new CopyWebpackPlugin([
-          { from: 'app/src/images', to:'./images' }
-        ])
+        new CopyWebpackPlugin([{ from: "app/src/images", to: "./images" }]),
+        new webpack.ProvidePlugin({
+          $:'jquery',
+          jQuery:'jquery'
+        })
       ]
     }
-  ])
-}
-
+  ]);
+};
