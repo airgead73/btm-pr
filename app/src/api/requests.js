f8bfabf76c1c58625875322ed75dc52005879843
Dan Moneypenny <catullus1973@gmail.com>
2019-07-22 22:47:41 -0700

    update request url

--- app/src/api/requests.js
+++ app/src/api/requests.js
@@ -3,15 +3,17 @@ const ADD_IMAGE = 'https://us-central1-btm-pr.cloudfunctions.net/addImage';
 const ADD_IMAGES = 'https://us-central1-btm-pr.cloudfunctions.net/addImages';
 const UPDATE_IMAGES = 'https://us-central1-btm-pr.cloudfunctions.net/updateImage';
 const DELETE_IMAGE = 'https://us-central1-btm-pr.cloudfunctions.net/deleteImage';
-//const FILTER_ONE = 'https://us-central1-btm-pr.cloudfunctions.net/filterOne';
-//const FILTER_MULTIPLE ='https://us-central1-btm-pr.cloudfunctions.net/filterMultiple';
+const FILTER_SINGLE = 'https://us-central1-btm-pr.cloudfunctions.net/filterSingle';
+const FILTER_MODALITY_CATEGORY ='https://us-central1-btm-pr.cloudfunctions.net/filterModalityCategory';
+const FILTER_MODALITY_MEDIUM ='https://us-central1-btm-pr.cloudfunctions.net/filterModalityMedium';
 
 module.exports = {
   GET_IMAGES,
   ADD_IMAGE,
   ADD_IMAGES,
   UPDATE_IMAGES,
-  DELETE_IMAGE
-  //FILTER_ONE,
-  //FILTER_MULTIPLE
+  DELETE_IMAGE,
+  FILTER_SINGLE,
+  FILTER_MODALITY_CATEGORY,
+  FILTER_MODALITY_MEDIUM
 }
