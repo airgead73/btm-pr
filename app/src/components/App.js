285526986f6e03195cad96d6bc2e122b92c25ba4
Dan Moneypenny <catullus1973@gmail.com>
2019-07-22 23:03:58 -0700

    add Nav

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,47 +1,45 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-// import { 
-//   FILTER_MULTIPLE
-//  } from '../api/requests';
+import { Route, Switch } from 'react-router-dom';
+import Nav from './Nav/Nav';
+import Home from './Views/Home';
+import About from './Views/About';
+import Work from './Views/Work';
+import WorkModality from './Views/WorkModality';
+import WorkModalityCategory from './Views/WorkModalityCategory';
+import Contact from './Views/Contact';
+import Terms from './Views/Terms';
+import PageNotFound from './Views/PageNotFound';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
-  }
 
-  state = {
-    images: [],
-    view: 'home'
-  }
-
-  // componentDidMount() {
-  //   axios({
-  //     method: 'get',
-  //     url: FILTER_MULTIPLE,
-  //     params: {
-  //       modality: "sculpture",
-  //       category: "abstract"
-  //     }
-  //   })
-  //   .then(res => {
-  //     this.setState({ images: res.data });
-  //   })
-  //   .catch(error => {
-  //     this.setState({ images: error.message });
-  //   });
-  // }
+    this.state = {
+      currentImages: [],
+      message: ''
+    }
+  }  
 
   render() {
-    const images = this.state.images;
-    console.log(images);
     return (
-      <React.Fragment>
-        <h1>App Home</h1>
-        <p className="txt-center">images</p>
+      <div>
+        <Nav/>
+        <h1>Brian's Work</h1>
+        <Switch>
+          <Route exact path='/'component={Home}/>
+          <Route path='/about' component={About}/>
+          <Route exact path='/work' component={Work}/>
+          <Route exact path='/work/:modality' component={WorkModality}/>
+          <Route path='/work/:modality/:category' component={WorkModalityCategory}/>
+          <Route path='/contact' component={Contact}/>
+          <Route path='/terms' component={Terms}/>
+          <Route component={PageNotFound}/>
+          
+        </Switch>
 
-
-      </React.Fragment>
-    )
+      </div>
+    );
   }
 }
 
