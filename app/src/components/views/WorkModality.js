be2c2f745f00d18bf502f9b212dfbf2a1942afb7
Dan Moneypenny <catullus1973@gmail.com>
2019-07-22 22:55:45 -0700

    add views

---
+++ app/src/components/views/WorkModality.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WorkModality = () => {
+  return (
+    <>
+    <h1>WorkModality Page</h1>
+    </>
+  );
+};
+
+export default WorkModality; 
---
+++ app/src/components/views/WorkModalityCategory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WorkModalityCategory = () => {
+  return (
+    <>
+    <h1>WorkModalityCategory Page</h1>
+    </>
+  );
+};
+
+export default WorkModalityCategory; 
